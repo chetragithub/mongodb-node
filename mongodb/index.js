b2ae4a47c75c71f5faaab5c5aa257f80909cc2df
ChetraGithub <111889662+ChetraGithub@users.noreply.github.com>
2023-12-01 16:59:58 +0700

    Update mongo URL

--- mongodb/index.js
+++ mongodb/index.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export default function initDb() {
   const dbURL =
-    'mongodb+srv://chetra-mongo-db:chetra12345@cluster0.bpdatys.mongodb.net/?retryWrites=true&w=majority'
+    'MONGO_URL'
   mongoose
     .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('connected to db'))
